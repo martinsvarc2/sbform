d51da325d9ab21f10660d2694394b9cc82466591
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-12-05 04:54:11 +0100

    Update multi-combobox.tsx

--- src/components/multi-combobox.tsx
+++ src/components/multi-combobox.tsx
@@ -61,7 +61,7 @@ export function MultiCombobox({
             <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
           </Button>
         </PopoverTrigger>
-        <PopoverContent className="w-full p-0">
+       <PopoverContent align="start" className="w-full p-0">
           <Command>
             <CommandInput placeholder="Search..." />
             <CommandEmpty>No results found.</CommandEmpty>
