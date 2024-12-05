7aaac73bdbf41bcdd062685926ae9251e390bf36
avamus <lukassys2004@gmail.com>
2024-12-05 05:15:39 +0100

    Update popover.tsx

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -4,7 +4,6 @@ import * as PopoverPrimitive from "@radix-ui/react-popover"
 import { cn } from "@/lib/utils"
 
 const Popover = PopoverPrimitive.Root
-
 const PopoverTrigger = PopoverPrimitive.Trigger
 
 const PopoverContent = React.forwardRef
