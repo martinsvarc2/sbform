d7734aaf471d6e03db22aa0dd3410b2066fb4051
avamus <lukassys2004@gmail.com>
2024-12-06 01:36:08 +0100

    Update form.ts

--- src/types/form.ts
+++ src/types/form.ts
@@ -1,20 +1,25 @@
-export type TargetingType = 'national' | 'state' | 'zipCode' | null
+export type TargetingType = 'national' | 'state' | 'zipCode' | null;
 
 export interface FormState {
-  firstName: string
-  lastName: string
-  email: string
-  phoneNumber: string
-  campaignName: string
-  targetingType: TargetingType
-  selectedStates: string[]
-  selectedCities: string[]
-  zipCodes: string[]
-  leadsPerDay: number
-  googleSheetUrl: string
-  webhookUrl: string
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  campaignName: string;
+  targetingType: TargetingType;
+  selectedStates: string[];
+  selectedCities: string[];
+  zipCodes: string[];
+  leadsPerDay: number;
+  googleSheetUrl: string;
+  webhookUrl: string;
 }
 
 export interface LocationFormProps {
-  onSubmit: (data: FormState) => void
+  onSubmit: (formData: FormState) => void;
+}
+
+export interface CitySelector {
+  onCitySelect: (city: { name: string }) => void;
+  selectedCities: string[];
 }
